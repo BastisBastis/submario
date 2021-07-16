157c92f3a1d514be67beb94b17493a33912f5cee
Bastis <sebgus@gmail.com>
2021-07-16 12:05:17 +0200

    new tilemap

---
+++ public/assets/smbstiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="smbstiles" tilewidth="16" tileheight="16" tilecount="448" columns="28">
+ <image source="smbstiles.png" width="448" height="256"/>
+</tileset>
