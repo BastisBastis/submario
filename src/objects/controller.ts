ab7f29db038cbcdb42d79592e4924bc3f83235c3
Bastis <sebgus@gmail.com>
2021-07-16 23:09:38 +0200

    asdf

--- src/objects/controller.ts
+++ src/objects/controller.ts
@@ -9,14 +9,14 @@ export default class controller {
         
         const leftArrow = scene.input.keyboard.addKey("LEFT");
         leftArrow.on("down", () => {
-            player.move(-1);
+            player.move(-1)
         });
         leftArrow.on("up", () => {
-            player.stop();
+            player.stop()
         }
         const rightArrow = scene.input.keyboard.addKey("RIGHT");
         rightArrow.on("down", () => {
-            player.move(1);
+            player.move(1)
         });
         rightArrow.on("up", () => {
             player.stop() 
