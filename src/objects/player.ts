305fed98979457ce298f01955b67176298c2d8de
Bastis <sebgus@gmail.com>
2021-07-16 23:15:52 +0200

    asdf

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -30,6 +30,7 @@ export default class Player {
     move(dir:number):void {
         this.sprite.play("run");
         this.sprite.setVelocityX(dir*this.maxSpeedX);
+        this.sprite.scaleX = dir;
     }
 
     stop():void {
