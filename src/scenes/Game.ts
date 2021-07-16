9e46292b7658862567dd90ca9026da1851df54c7
Bastis <sebgus@gmail.com>
2021-07-16 20:57:23 +0200

    testing sprite

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -36,5 +36,6 @@ export default class Demo extends Phaser.Scene {
     this.physics.world.bounds.width = this.groundLayer.width;
     this.physics.world.bounds.height = this.groundLayer.height;
 
+    this.add.sprite(20,20,"mariostop");
   }
 }
