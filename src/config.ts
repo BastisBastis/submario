f4f048d904fcd82181ce798fcbaf3fc6c23db79a
Bastis <sebgus@gmail.com>
2021-07-16 10:41:19 +0200

    background color change

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#888888ff',
+  backgroundColor: '#8888ff',
   scale: {
     width: 800,
     height: 600,
